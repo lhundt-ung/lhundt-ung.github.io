224f853f6a748551db05d1337af1d1ab8439062a
Lance Hundt <lance.hundt@ung.edu>
2020-12-07 23:27:46 -0500

    Updated custom.js 2

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -18,16 +18,26 @@ window.onload =
           }
         }); };
     
-
+*/
 //edit
 window.onload = function() {
-    $(function(){ alert("jQuery + DOM loaded."); });
-}*/
+    var anchors = document.querySelectorAll('a[href*="mybinder.org"]');
+    Array.prototype.forEach.call(anchors, function (element, index) {
+        element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub-dev.ung.co/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
+        var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub-dev.ung.co/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
+        element.href = newhref;
+    });
+}
 
-var anchors = document.querySelectorAll('a[href*="mybinder.org"]');
-Array.prototype.forEach.call(anchors, function (element, index) {
-    element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub-dev.ung.co/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
-    var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub-dev.ung.co/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
-    element.href = newhref;
-});
+window.addEventListener('load', function () {
+    var anchors = document.querySelectorAll('a[href*="mybinder.org"]');
+    Array.prototype.forEach.call(anchors, function (element, index) {
+        element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub-dev.ung.co/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
+        var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub-dev.ung.co/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
+        element.href = newhref;
+    });
+  })
+
+
+
 
