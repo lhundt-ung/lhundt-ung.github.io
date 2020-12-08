551b84b7eaa2fca38b4e03d6dfbd3f6d497f1adf
Lance Hundt <lance.hundt@ung.edu>
2020-12-07 23:18:14 -0500

    Updated custom.js 2

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -26,7 +26,8 @@ window.onload = function() {
 
 var anchors = document.querySelectorAll('a[href*="mybinder.org"]');
 Array.prototype.forEach.call(anchors, function (element, index) {
+    element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub-dev.ung.co/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
     var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub-dev.ung.co/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
-    element.href = newhref;
+    element.href = 'https://ung.edu';
 });
 
