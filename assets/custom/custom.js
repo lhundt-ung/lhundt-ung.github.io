071451b0ca9c2a05c9537e33a5bb49e29cce8d68
Lance Hundt <lance.hundt@ung.edu>
2020-12-07 22:27:41 -0500

    Updated custom.js 2

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -1,28 +1,5 @@
-// Put your custom javascript here
-function include(filename, onload) {
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = filename;
-    script.type = 'text/javascript';
-    script.onload = script.onreadystatechange = function() {
-        if (script.readyState) {
-            if (script.readyState === 'complete' || script.readyState === 'loaded') {
-                script.onreadystatechange = null;                                                  
-                onload();
-            }
-        } 
-        else {
-            onload();          
-        }
-    };
-    head.appendChild(script);
-}
 
-include('https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', function() {
-    $(document).ready(function() {
-        alert('the DOM is ready');
-    });
-});
+import $ from 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
 
 var href = null;
 $("a").each(function()
